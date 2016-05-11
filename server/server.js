e3be6b5e4130156abe0499a2272d9127e29ec079
Sergey Piterman <sergeypiterman@gmail.com>
2016-05-11 12:37:15 -0700

    [feat] Add middleware

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
+'use strict'
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
 
 app.use(express.static(__dirname + "/../client"));
+
+require('./middleware/middleware.js')(app, express);
 
 app.listen(port);
 console.log('Server listening on ' + port);
