a4de00070f5b05f6079abe76fbe3984a32280349
Xavier <hayeck.xavier@gmail.com>
2016-05-11 12:41:25 -0700

    created home.js

---
+++ client/home/home.js
@@ -0,0 +1,13 @@
+angular.module('neighborhoodwatch.home', [])
+
+.controller('homeScreen', function($scope, $state) {
+
+  $scope.signIn = function() {
+    $state.go('signIn');
+  };
+
+  $scope.signUp = function() {
+    $state.go('newUser');
+  };
+  
+});
