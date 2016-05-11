85a4dcf19a9dfc618ca9736513bf2a239e3644fc
Xavier <hayeck.xavier@gmail.com>
2016-05-11 12:20:50 -0700

    created app.js

---
+++ client/app.js
@@ -0,0 +1,14 @@
+angular.module()
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'home/home.html',
+      controller: 'homeScreen'
+    });
+    
+});
