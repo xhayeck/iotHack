30a035b04b06ae4f88f1d955f1297dc06142f4da
Malek Ascha <malekascha@gmail.com>
2016-05-12 09:49:52 -0700

    Revert "Revert "Factories""

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const db = require('./db.js');
+const app = express();
 
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + "/../client"));
 app.use(express.static(__dirname + "/../node_modules"));
 
